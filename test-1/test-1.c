6c395f6b5117722aaaec16141d6c712717533fe1
Matcha-Maffin <139634366+Matcha-Maffin@users.noreply.github.com>
2023-07-16 23:45:47 +0800

    Add files via upload

---
+++ test-1/test-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	//printf("100\n");
+	//printf("%d", 100);
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(short));
+	char ch = 'a';
+
+	return  0;
+
+}
